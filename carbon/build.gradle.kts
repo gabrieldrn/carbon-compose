7a56491b11fa38973491020897a002c6b2b819b6
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2023-11-02 00:26:19 -0400

    Changed focus animation implementation by width instead of offset animation

--- carbon/build.gradle.kts
+++ carbon/build.gradle.kts
@@ -10,4 +10,6 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.uiTooling)
     api(libs.androidx.compose.foundation)
+
+//    implementation(libs.timber)
 }
--- carbon/src/main/java/dev/gabrieldrn/carbon/button/ButtonIndication.kt
+++ carbon/src/main/java/dev/gabrieldrn/carbon/button/ButtonIndication.kt
@@ -5,10 +5,11 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Indication
 import androidx.compose.foundation.IndicationInstance
 import androidx.compose.foundation.interaction.FocusInteraction
-import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.referentialEqualityPolicy
 import androidx.compose.runtime.remember
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -22,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import dev.gabrieldrn.carbon.color.LocalCarbonTheme
 import dev.gabrieldrn.carbon.color.Theme
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.launch
 
 internal class ButtonIndication(
@@ -34,19 +36,38 @@ internal class ButtonIndication(
     ) : IndicationInstance {
 
         private val borderFocusColor = theme.focus
-        private val insetFocusColor =
-            if (buttonType == CarbonButton.Ghost) {
-                Color.Transparent
-            } else {
-                theme.focusInset
+        private val insetFocusColor = if (buttonType == CarbonButton.Ghost) {
+            Color.Transparent
+        } else {
+            theme.focusInset
+        }
+
+        private val borderFocusColorState = mutableStateOf(
+            value = Color.Transparent,
+            policy = referentialEqualityPolicy()
+        )
+        private val insetFocusColorState = mutableStateOf(
+            value = Color.Transparent,
+            policy = referentialEqualityPolicy()
+        )
+
+        private val focusAnimation = Animatable(0f)
+
+        fun animateFocus(scope: CoroutineScope, interaction: FocusInteraction) {
+            scope.launch {
+                // Strangely, when the focus border is multiplied by 0, there is still a border
+                // visible. This is why the border is set to transparent when the focus is lost.
+                if (interaction is FocusInteraction.Focus) {
+                    borderFocusColorState.value = borderFocusColor
+                    insetFocusColorState.value = insetFocusColor
+                } else {
+                    borderFocusColorState.value = Color.Transparent
+                    insetFocusColorState.value = Color.Transparent
+                }
             }
-
-        private val focusAnimation = Animatable(1f)
-
-        fun animateFocus(scope: CoroutineScope, interaction: Interaction) {
             scope.launch {
                 focusAnimation.animateTo(
-                    targetValue = if (interaction is FocusInteraction.Focus) 0f else 1f,
+                    targetValue = if (interaction is FocusInteraction.Focus) 1f else 0f,
                     animationSpec = tween(
                         durationMillis = ButtonTransitionDurationMillis,
                         easing = buttonTransitionEasing
@@ -61,45 +82,33 @@ internal class ButtonIndication(
         }
 
         private fun DrawScope.drawFocus() {
-            val borderStrokeWidthPx = with(FOCUS_BORDER_WIDTH.dp) { toPx() }
-            val insetStrokeWidthPx = with(FOCUS_INSET_WIDTH.dp) { toPx() }
+            val borderStrokeWidthPx = (FOCUS_BORDER_WIDTH * focusAnimation.value).dp.toPx()
+            val insetStrokeWidthPx = (FOCUS_INSET_WIDTH * focusAnimation.value).dp.toPx()
 
             val borderHalfStroke = borderStrokeWidthPx / 2
-            val insetOffset = borderStrokeWidthPx + (borderHalfStroke / 2)
-
-            val borderStrokeOffset = Offset(borderStrokeWidthPx, borderStrokeWidthPx)
-            val insetStrokeOffset = borderStrokeOffset + Offset(insetOffset / 2, insetOffset / 2)
+            val insetOffset = borderStrokeWidthPx + borderHalfStroke / 2
 
             val topLeft = Offset(borderHalfStroke, borderHalfStroke)
-                .minus(borderStrokeOffset * focusAnimation.value)
             val topLeftInset = Offset(insetOffset, insetOffset)
-                .minus(insetStrokeOffset * focusAnimation.value)
 
-            val borderSize = (borderStrokeWidthPx * 2 * focusAnimation.value).let { animOffset ->
-                Size(
-                    size.width - borderStrokeWidthPx + animOffset,
-                    size.height - borderStrokeWidthPx + animOffset
-                )
-            }
-            val insetSize = (
-                insetOffset *
-                    2.60f * // A magic number never heart a button, right?
-                    focusAnimation.value
-                ).let { animOffset ->
-                    Size(
-                        size.width - insetOffset * 2 + animOffset,
-                        size.height - insetOffset * 2 + animOffset
-                    )
-                }
+            val borderSize = Size(
+                size.width - borderStrokeWidthPx,
+                size.height - borderStrokeWidthPx
+            )
+            val insetSize = Size(
+                size.width - insetOffset * 2,
+                size.height - insetOffset * 2
+            )
+
             // The inset is drawn first to make it appear below the border while animating
             drawRect(
-                brush = SolidColor(insetFocusColor),
+                brush = SolidColor(insetFocusColorState.value),
                 topLeft = topLeftInset,
                 size = insetSize,
                 style = Stroke(insetStrokeWidthPx)
             )
             drawRect(
-                brush = SolidColor(borderFocusColor),
+                brush = SolidColor(borderFocusColorState.value),
                 topLeft = topLeft,
                 size = borderSize,
                 style = Stroke(borderStrokeWidthPx)
@@ -119,13 +128,16 @@ internal class ButtonIndication(
         val theme = LocalCarbonTheme.current
 
         val instance = remember(theme) {
+            // TODO Does it still work when the theme changes on runtime?
             ButtonIndicationInstance(theme = theme, buttonType = buttonType)
         }
 
         LaunchedEffect(interactionSource) {
-            interactionSource.interactions.collect { interaction ->
-                instance.animateFocus(this, interaction)
-            }
+            interactionSource.interactions
+                .filterIsInstance<FocusInteraction>()
+                .collect { interaction ->
+                    instance.animateFocus(this, interaction)
+                }
         }
 
         return instance
