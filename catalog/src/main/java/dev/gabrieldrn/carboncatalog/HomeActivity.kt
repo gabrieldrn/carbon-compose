9e8f45c82eff6d63418c191d94142b03484f3c7d
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2023-11-03 23:09:29 -0400

    Added edge-to-edge in demo contents

--- catalog/src/main/java/dev/gabrieldrn/carboncatalog/HomeActivity.kt
+++ catalog/src/main/java/dev/gabrieldrn/carboncatalog/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -35,6 +36,9 @@ class HomeActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContent {
             CarbonDesignSystem {
                 Column(
--- catalog/src/main/java/dev/gabrieldrn/carboncatalog/buttons/ButtonsDemoActivity.kt
+++ catalog/src/main/java/dev/gabrieldrn/carboncatalog/buttons/ButtonsDemoActivity.kt
@@ -2,6 +2,7 @@ package dev.gabrieldrn.carboncatalog.buttons
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -45,6 +46,9 @@ class ButtonsDemoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContent {
             CarbonDesignSystem {
                 Column(
