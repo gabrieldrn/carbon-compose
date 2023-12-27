a5d7e2f91a1ec01690fbaa9f3b0d75fe639ff49e
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2023-12-27 15:16:17 -0500

    Added Dropdown demo activity in catalog

---
+++ catalog/src/main/kotlin/carbon/compose/catalog/dropdown/DropdownDemoActivity.kt
@@ -0,0 +1,58 @@
+package carbon.compose.catalog.dropdown
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import carbon.compose.catalog.R
+import carbon.compose.catalog.theme.CarbonCatalogTheme
+import carbon.compose.dropdown.Dropdown
+import carbon.compose.foundation.color.LocalCarbonTheme
+import carbon.compose.uishell.UiShellHeader
+
+class DropdownDemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT))
+
+        setContent {
+            CarbonCatalogTheme {
+                Column(
+                    modifier = Modifier.background(LocalCarbonTheme.current.background),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    UiShellHeader(
+                        headerName = "Checkbox",
+                        menuIconRes = R.drawable.ic_arrow_left,
+                        onMenuIconPressed = { onBackPressedDispatcher.onBackPressed() },
+                    )
+
+                    Box(modifier = Modifier.weight(1f).fillMaxWidth()) {
+                        Dropdown(
+                            fieldPlaceholderText = "Dropdown",
+                            optionSelected = null,
+                            options = (0..5).associateWith { "Option $it" },
+                            onOptionSelected = {},
+                            modifier = Modifier
+                                .width(200.dp)
+                                .padding(8.dp),
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
