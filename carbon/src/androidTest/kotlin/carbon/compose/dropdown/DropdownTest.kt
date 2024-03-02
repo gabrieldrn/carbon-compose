bb3ed7bade0b31a285c694de4497175903f3cfe2
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-03-02 16:39:54 -0500

    Added dropdown focus + expand/shrink ui tests

--- carbon/src/androidTest/kotlin/carbon/compose/dropdown/DropdownTest.kt
+++ carbon/src/androidTest/kotlin/carbon/compose/dropdown/DropdownTest.kt
@@ -8,13 +8,17 @@ import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertHeightIsEqualTo
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsFocused
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollToNode
+import androidx.compose.ui.test.requestFocus
 import androidx.compose.ui.unit.dp
 import carbon.compose.toList
 import org.junit.Rule
@@ -72,6 +76,31 @@ class DropdownTest {
     }
 
     @Test
+    fun dropdown_field_validateFocusAbility() {
+        val isExpanded = mutableStateOf(false)
+        composeTestRule.setContent {
+            Column(Modifier.fillMaxWidth()) {
+                Dropdown(
+                    expanded = isExpanded.value,
+                    fieldPlaceholderText = "Dropdown",
+                    selectedOption = null,
+                    options = options,
+                    onOptionSelected = {},
+                    onExpandedChange = { isExpanded.value = it },
+                    onDismissRequest = { isExpanded.value = false },
+                    minVisibleItems = minVisibleItems
+                )
+            }
+        }
+
+        composeTestRule.run {
+            onNodeWithTag(DropdownTestTags.FIELD)
+                .requestFocus()
+                .assertIsFocused()
+        }
+    }
+
+    @Test
     fun dropdown_optionsPopup_validateLayout() {
         val isExpanded = mutableStateOf(false)
         composeTestRule.setContent {
@@ -105,6 +134,88 @@ class DropdownTest {
 
             onNode(hasTestTag(DropdownTestTags.MENU_OPTION).and(hasText("Option 9")))
                 .assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun dropdown_optionsPopup_expandAndCollapse() {
+        val isExpanded = mutableStateOf(false)
+        composeTestRule.setContent {
+            Column(Modifier.fillMaxWidth()) {
+                Dropdown(
+                    expanded = isExpanded.value,
+                    fieldPlaceholderText = "Dropdown",
+                    selectedOption = null,
+                    options = options,
+                    onOptionSelected = {},
+                    onExpandedChange = { isExpanded.value = it },
+                    onDismissRequest = { isExpanded.value = false },
+                    minVisibleItems = minVisibleItems
+                )
+            }
+        }
+
+        composeTestRule.run {
+            onNodeWithTag(DropdownTestTags.POPUP_CONTENT)
+                .assertIsNotDisplayed()
+
+            onNodeWithTag(DropdownTestTags.FIELD)
+                .performClick()
+
+            onNodeWithTag(DropdownTestTags.POPUP_CONTENT)
+                .assertIsDisplayed()
+
+            // TODO Tried to shrink the dropdown by invoking a touch event on the field, but for
+            //  some reason it didn't work.
+//            onNodeWithTag(DropdownTestTags.FIELD)
+//                .performClick()
+            isExpanded.value = false
+
+            onNodeWithTag(DropdownTestTags.POPUP_CONTENT)
+                .assertIsNotDisplayed()
+        }
+    }
+
+    @Test
+    fun dropdown_optionsPopup_validateFocusAbility() {
+        val isExpanded = mutableStateOf(false)
+        val selectedOptionKey = mutableStateOf<Int?>(null)
+        composeTestRule.setContent {
+            Column(Modifier.fillMaxWidth()) {
+                Dropdown(
+                    expanded = isExpanded.value,
+                    fieldPlaceholderText = "Dropdown",
+                    selectedOption = selectedOptionKey.value,
+                    options = options,
+                    onOptionSelected = { selectedOptionKey.value = it },
+                    onExpandedChange = { isExpanded.value = it },
+                    onDismissRequest = { isExpanded.value = false },
+                    minVisibleItems = minVisibleItems
+                )
+            }
+        }
+
+        composeTestRule.run {
+            onNodeWithTag(DropdownTestTags.FIELD)
+                .requestFocus()
+                .performClick()
+
+            waitForIdle()
+
+            onAllNodesWithTag(DropdownTestTags.MENU_OPTION).run {
+                // Check that the first option is focused.
+                onFirst().assertIsFocused()
+                // Select option 4.
+                get(4).performClick()
+            }
+
+            // Reopen the dropdown.
+            onNodeWithTag(DropdownTestTags.FIELD).performClick()
+
+            // Check that the previously selected option is focused.
+            onAllNodesWithTag(DropdownTestTags.MENU_OPTION)
+                .onFirst() // The first node should be the selected option.
+                .assertIsFocused()
         }
     }
 }
