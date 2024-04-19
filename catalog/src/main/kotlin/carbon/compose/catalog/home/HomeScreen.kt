cef3306eb97cb2cfe3cf7b039a25d16519f5972c
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-04-19 18:38:35 -0400

    Added appearance animation on tiles in home screen

--- catalog/src/main/kotlin/carbon/compose/catalog/home/HomeScreen.kt
+++ catalog/src/main/kotlin/carbon/compose/catalog/home/HomeScreen.kt
@@ -1,6 +1,8 @@
 package carbon.compose.catalog.home
 
 import android.content.res.Configuration
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -23,17 +25,25 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import carbon.compose.catalog.R
 import carbon.compose.foundation.color.LocalCarbonTheme
+import carbon.compose.foundation.motion.Motion
 import carbon.compose.foundation.spacing.SpacingScale
 import carbon.compose.foundation.text.CarbonTypography
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
@@ -124,34 +134,73 @@ private fun ComponentsLazyGrid(
     }
 }
 
+private val itemAppearanceAnimationSpec = tween<Float>(
+    delayMillis = Motion.Duration.moderate01,
+    durationMillis = Motion.Duration.slow01,
+    easing = Motion.Entrance.expressiveEasing
+)
+
+private val itemInitOffset = -16f
+
 @Composable
 private fun CarbonComponentGridTile(
     destination: Destination,
     onTileClicked: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    var yOffsetDp by remember { mutableFloatStateOf(itemInitOffset) }
+    var alpha by remember { mutableFloatStateOf(0f) }
+    LaunchedEffect(destination) {
+        launch {
+            animate(
+                initialValue = itemInitOffset,
+                targetValue = 0f,
+                animationSpec = itemAppearanceAnimationSpec
+            ) { value, _ ->
+                yOffsetDp = value
+            }
+        }
+        launch {
+            animate(
+                initialValue = 0f,
+                targetValue = 1f,
+                animationSpec = itemAppearanceAnimationSpec
+            ) { value, _ ->
+                alpha = value
+            }
+        }
+    }
     Box(
         modifier = modifier
             .clickable(onClick = onTileClicked)
             .background(color = LocalCarbonTheme.current.layer01)
             .fillMaxSize()
     ) {
-        if (destination.illustration != null) {
-            Image(
-                painter = painterResource(id = destination.illustration),
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer {
+                    translationY = yOffsetDp.dp.toPx()
+                    this.alpha = alpha
+                }
+        ) {
+            if (destination.illustration != null) {
+                Image(
+                    painter = painterResource(id = destination.illustration),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
+            } else {
+                PlaceholderIllustration()
+            }
+            BasicText(
+                text = destination.title,
+                style = CarbonTypography.body01.copy(
+                    color = LocalCarbonTheme.current.textPrimary
+                ),
+                modifier = Modifier.padding(SpacingScale.spacing05)
             )
-        } else {
-            PlaceholderIllustration()
         }
-        BasicText(
-            text = destination.title,
-            style = CarbonTypography.body01.copy(
-                color = LocalCarbonTheme.current.textPrimary
-            ),
-            modifier = Modifier.padding(SpacingScale.spacing05)
-        )
     }
 }
 
