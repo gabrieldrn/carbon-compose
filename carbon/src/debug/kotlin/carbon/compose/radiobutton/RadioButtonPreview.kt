b143e9db54021072e6308cc03b60a4be23808a8a
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-04-28 16:42:14 -0400

    Impl radio button icon component drawing + init composable api

--- carbon/src/debug/kotlin/carbon/compose/radiobutton/RadioButtonPreview.kt
+++ carbon/src/debug/kotlin/carbon/compose/radiobutton/RadioButtonPreview.kt
@@ -1,1 +1,17 @@
 package carbon.compose.radiobutton
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+private fun RadioButtonIconPreview() {
+    Row {
+        RadioButton(checked = false, onClick = {})
+        RadioButton(checked = true, onClick = {}, modifier = Modifier.padding(start = 2.dp))
+    }
+}
--- carbon/src/main/kotlin/carbon/compose/radiobutton/RadioButton.kt
+++ carbon/src/main/kotlin/carbon/compose/radiobutton/RadioButton.kt
@@ -1,13 +1,49 @@
 package carbon.compose.radiobutton
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
 
 @Composable
 public fun RadioButton(
-    selected: Boolean,
+    checked: Boolean,
     onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    errorMessage: String = "",
+    warningMessage: String = "",
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+) {
+    RadioButtonComponent(
+        checked = checked,
+        modifier = modifier
+    )
+}
+
+private val RadioButtonSize = 20.dp
+private val RadioButtonStrokeWidth = 1.dp
+
+@Composable
+private fun RadioButtonComponent(
+    checked: Boolean,
     modifier: Modifier = Modifier
 ) {
-
+    Canvas(modifier = modifier.requiredSize(RadioButtonSize)) {
+        drawCircle(
+            color = Color.Black,
+            radius = (size.width - RadioButtonStrokeWidth.toPx()) * .5f,
+            style = Stroke(width = RadioButtonStrokeWidth.toPx())
+        )
+        if (checked) {
+            drawCircle(
+                color = Color.Black,
+                radius = size.width * .25f,
+            )
+        }
+    }
 }
