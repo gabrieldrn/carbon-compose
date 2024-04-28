e25c36729434e2b07091730c6db409396fd36cd3
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-04-28 17:49:43 -0400

    Init kdoc

--- carbon/src/main/kotlin/carbon/compose/radiobutton/RadioButton.kt
+++ carbon/src/main/kotlin/carbon/compose/radiobutton/RadioButton.kt
@@ -10,6 +10,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 
+/**
+ * # Carbon Radio button
+ * Radio buttons are used for mutually exclusive choices, not for multiple choices. Only one radio
+ * button can be selected at a time. When a user chooses a new item, the previous choice is
+ * automatically deselected.
+ *
+ * ## Anatomy
+ * The radio button component is comprised of a set of clickable circles (the inputs) with text
+ * labels positioned to the right. If there is a group of radio buttons, a group label can be added.
+ *
+ * (From [Radio button documentation](https://carbondesignsystem.com/components/radio-button/usage))
+ */
 @Composable
 public fun RadioButton(
     checked: Boolean,
