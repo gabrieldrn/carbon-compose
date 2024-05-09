0b6e88492d9b1604ddd38404feb5087faa8b5b4e
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-05-09 01:33:56 -0400

    Impl small loading component + improved demo

--- carbon/src/debug/kotlin/carbon/compose/loading/LoadingSpinnerPreview.kt
+++ carbon/src/debug/kotlin/carbon/compose/loading/LoadingSpinnerPreview.kt
@@ -1,14 +1,16 @@
 package carbon.compose.loading
 
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
 private fun LoadingSpinnerPreview() {
     Loading()
+}
+
+@Preview
+@Composable
+private fun SmallLoadingSpinnerPreview() {
+    SmallLoading()
 }
