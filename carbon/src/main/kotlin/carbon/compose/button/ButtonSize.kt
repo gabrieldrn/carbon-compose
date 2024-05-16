3f38b09ff73c66e1e02574df19e006b6bb966541
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-05-16 00:54:56 -0400

    Added KDoc to ButtonSize

--- carbon/src/main/kotlin/carbon/compose/button/ButtonSize.kt
+++ carbon/src/main/kotlin/carbon/compose/button/ButtonSize.kt
@@ -6,22 +6,53 @@ import androidx.compose.ui.unit.dp
 import carbon.compose.foundation.SMALL_TOUCH_TARGET_SIZE_MESSAGE
 import carbon.compose.foundation.spacing.SpacingScale
 
+/**
+ * Enum class representing different sizes for a button.
+ * Each size has an associated height in dp.
+ *
+ * @property height The height of the button in dp.
+ */
 public enum class ButtonSize(internal val height: Dp) {
 
+    /**
+     * Use when there is not enough vertical space for the default or field-sized button.
+     */
     @Deprecated(
         SMALL_TOUCH_TARGET_SIZE_MESSAGE,
         ReplaceWith("LargeProductive")
     )
     Small(height = 32.dp),
 
+    /**
+     * Use when buttons are paired with input fields.
+     */
     @Deprecated(
         SMALL_TOUCH_TARGET_SIZE_MESSAGE,
         ReplaceWith("LargeProductive")
     )
     Medium(height = 40.dp),
+
+    /**
+     * This is the most common button size.
+     */
     LargeProductive(height = 48.dp),
+
+    /**
+     * The larger expressive type size within this button provides balance when used with 16sp body
+     * copy. Used by the IBM.com team in website banners.
+     */
     LargeExpressive(height = 48.dp),
+
+    /**
+     * Use when buttons bleed to the edge of a larger component, like side panels or modals.
+     */
+    // TODO: Exclude this from public API, it should only be offered inside a modal.
     ExtraLarge(height = 64.dp),
+
+    /**
+     * Use when buttons bleed to the edge of a larger component, like side panels or modals.
+     */
+    // TODO: Exclude this from public API, it should only be offered inside a modal.
     TwiceExtraLarge(height = 80.dp);
 
     internal companion object {
