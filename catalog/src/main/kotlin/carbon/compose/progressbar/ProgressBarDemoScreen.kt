eb80bc3e5475eb57b573c054a189554d080db9ac
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-06-02 20:43:44 -0400

    Added progress bar state to demo

--- catalog/src/main/kotlin/carbon/compose/progressbar/ProgressBarDemoScreen.kt
+++ catalog/src/main/kotlin/carbon/compose/progressbar/ProgressBarDemoScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,29 +19,41 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import carbon.compose.dropdown.Dropdown
+import carbon.compose.dropdown.base.DropdownInteractiveState
 import carbon.compose.dropdown.base.DropdownOption
 import carbon.compose.foundation.color.CarbonLayer
 import carbon.compose.foundation.color.containerBackground
 import carbon.compose.foundation.spacing.SpacingScale
 import carbon.compose.toggle.Toggle
 
+private val progressBarStateOptions = ProgressBarState
+    .entries
+    .associateWith { DropdownOption(it.name) }
+
 private val progressBarVariantOptions = mapOf(
-    true to DropdownOption("Indeterminate"),
-    false to DropdownOption("Determinate")
+    false to DropdownOption("Determinate"),
+    true to DropdownOption("Indeterminate")
 )
 
-private val progressBarSizeOptions = mapOf(
-    ProgressBarSize.Big to DropdownOption("Big"),
-    ProgressBarSize.Small to DropdownOption("Small")
-)
+private val progressBarSizeOptions = ProgressBarSize
+    .entries
+    .associateWith { DropdownOption(it.name) }
 
 @Composable
 fun ProgressBarDemoScreen(modifier: Modifier = Modifier) {
+    var state by remember { mutableStateOf<ProgressBarState>(ProgressBarState.Active) }
     var isIndeterminate by remember { mutableStateOf(false) }
-    var progressBarSize by remember { mutableStateOf<ProgressBarSize>(ProgressBarSize.Small) }
+    var size by remember { mutableStateOf<ProgressBarSize>(ProgressBarSize.Small) }
     var isInlined by remember { mutableStateOf(false) }
     var isIndented by remember { mutableStateOf(false) }
     var progressBarVariantDropdownExpanded by remember { mutableStateOf(false) }
+    var progressBarStateDropdownExpanded by remember { mutableStateOf(false) }
+    val progressBarStateState by remember {
+        derivedStateOf {
+            if (isIndeterminate) DropdownInteractiveState.Disabled
+            else DropdownInteractiveState.Enabled
+        }
+    }
     var progressBarSizeDropdownExpanded by remember { mutableStateOf(false) }
 
     Column(
@@ -55,7 +68,7 @@ fun ProgressBarDemoScreen(modifier: Modifier = Modifier) {
                 IndeterminateProgressBar(
                     labelText = "Label text",
                     helperText = "Helper text",
-                    size = progressBarSize,
+                    size = size,
                     indented = isIndented,
                     inlined = isInlined,
                     modifier = Modifier.align(Alignment.Center)
@@ -65,9 +78,10 @@ fun ProgressBarDemoScreen(modifier: Modifier = Modifier) {
                     value = .75f,
                     labelText = "Label text",
                     helperText = "Helper text",
-                    size = progressBarSize,
+                    size = size,
                     indented = isIndented,
                     inlined = isInlined,
+                    state = state,
                     modifier = Modifier.align(Alignment.Center)
                 )
             }
@@ -81,7 +95,6 @@ fun ProgressBarDemoScreen(modifier: Modifier = Modifier) {
                     .padding(SpacingScale.spacing05),
                 verticalArrangement = Arrangement.spacedBy(SpacingScale.spacing05)
             ) {
-
                 Dropdown(
                     expanded = progressBarVariantDropdownExpanded,
                     placeholder = "Progress bar variant",
@@ -94,12 +107,24 @@ fun ProgressBarDemoScreen(modifier: Modifier = Modifier) {
                 )
 
                 Dropdown(
+                    expanded = progressBarStateDropdownExpanded,
+                    placeholder = "Progress bar state",
+                    label = "State",
+                    options = progressBarStateOptions,
+                    selectedOption = state,
+                    state = progressBarStateState,
+                    onOptionSelected = { option -> state = option },
+                    onExpandedChange = { progressBarStateDropdownExpanded = it },
+                    onDismissRequest = { progressBarStateDropdownExpanded = false }
+                )
+
+                Dropdown(
                     expanded = progressBarSizeDropdownExpanded,
                     placeholder = "Progress bar size",
                     label = "Size",
                     options = progressBarSizeOptions,
-                    selectedOption = progressBarSize,
-                    onOptionSelected = { option -> progressBarSize = option },
+                    selectedOption = size,
+                    onOptionSelected = { option -> size = option },
                     onExpandedChange = { progressBarSizeDropdownExpanded = it },
                     onDismissRequest = { progressBarSizeDropdownExpanded = false }
                 )
