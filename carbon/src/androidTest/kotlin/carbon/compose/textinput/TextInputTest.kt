0733ba43c254bd985993661237aa67b24a7a0315
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-06-28 00:32:17 -0400

    Instrumented tests for text area variant

--- carbon/src/androidTest/kotlin/carbon/compose/textinput/TextInputTest.kt
+++ carbon/src/androidTest/kotlin/carbon/compose/textinput/TextInputTest.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import carbon.compose.CarbonDesignSystem
 import carbon.compose.semantics.isReadOnly
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,6 +20,7 @@ import org.junit.runners.Parameterized
 
 @RunWith(Parameterized::class)
 class TextInputTest(
+    private val variant: TextInputVariant,
     private val value: String,
     private val placeholderText: String,
     private val helperText: String,
@@ -30,26 +30,34 @@ class TextInputTest(
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Before
-    fun setup() {
+    @Test
+    @Suppress("All")
+    fun textInput_validateLayout() {
         composeTestRule.setContent {
             CarbonDesignSystem {
-                TextInput(
-                    label = "Label", // The label is not parameterized as it's a mandatory element.
-                    value = value,
-                    onValueChange = {},
-                    placeholderText = placeholderText,
-                    helperText = helperText,
-                    state = state,
-                    // Size is not tested as only one size is technically supported
-                )
+                when (variant) {
+                    TextInputVariant.INPUT -> TextInput(
+                        // The label is not parameterized as it's a mandatory element.
+                        label = "Label",
+                        value = value,
+                        onValueChange = {},
+                        placeholderText = placeholderText,
+                        helperText = helperText,
+                        state = state,
+                        // Size is not tested as only one size is technically supported
+                    )
+                    TextInputVariant.AREA -> TextArea(
+                        label = "Label",
+                        value = value,
+                        onValueChange = {},
+                        placeholderText = placeholderText,
+                        helperText = helperText,
+                        state = state,
+                    )
+                }
             }
         }
-    }
 
-    @Test
-    @Suppress("CognitiveComplexMethod", "CyclomaticComplexMethod", "NestedBlockDepth")
-    fun textInput_validateLayout() {
         composeTestRule.run {
             onNodeWithTag(TextInputTestTags.LABEL, useUnmergedTree = true).run {
                 assertIsDisplayed()
@@ -98,6 +106,20 @@ class TextInputTest(
 
     @Test
     fun textInput_validateSemantics() {
+        composeTestRule.setContent {
+            CarbonDesignSystem {
+                TextInput(
+                    label = "Label", // The label is not parameterized as it's a mandatory element.
+                    value = value,
+                    onValueChange = {},
+                    placeholderText = placeholderText,
+                    helperText = helperText,
+                    state = state,
+                    // Size is not tested as only one size is technically supported
+                )
+            }
+        }
+
         composeTestRule.run {
             // Field state
             onNodeWithTag(TextInputTestTags.FIELD, useUnmergedTree = true).run {
@@ -118,32 +140,49 @@ class TextInputTest(
 
     companion object {
 
+        private val loremIpsum =
+            """
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                deserunt mollit anim id est laborum
+            """.trimIndent().replace("\n","")
+
         @Suppress("NestedBlockDepth")
         @JvmStatic
         @Parameterized.Parameters(
-            name = "value={0}, placeholderText={1}, helperText={2}, state={3}"
+            name = "textInputVariant={0}, value={1}, placeholderText={2}, helperText={3}, state={4}"
         )
         fun parameters(): Iterable<Array<Any?>> {
             val parameters = mutableListOf<Array<Any?>>()
 
-            listOf("", "Value").forEach { value ->
-                listOf("", "Placeholder").forEach { placeholderText ->
-                    listOf("", "Helper").forEach { helperText ->
-                        TextInputState.entries.forEach { state ->
-                            parameters.add(
-                                arrayOf(
-                                    value,
-                                    placeholderText,
-                                    helperText,
-                                    state,
+            TextInputVariant.entries.forEach { variant ->
+                listOf("", loremIpsum).forEach { value ->
+                    listOf("", "Placeholder").forEach { placeholderText ->
+                        listOf("", "Helper").forEach { helperText ->
+                            TextInputState.entries.forEach { state ->
+                                parameters.add(
+                                    arrayOf(
+                                        variant,
+                                        value,
+                                        placeholderText,
+                                        helperText,
+                                        state,
+                                    )
                                 )
-                            )
+                            }
                         }
                     }
                 }
             }
 
             return parameters
+        }
+
+        enum class TextInputVariant {
+            INPUT, AREA
         }
     }
 }
