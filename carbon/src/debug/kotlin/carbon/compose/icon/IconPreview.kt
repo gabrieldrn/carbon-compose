6fc746e05ce0e695d19dd85e986a892a16546168
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-06-30 20:25:08 -0400

    Added View and View Off icons

---
+++ carbon/src/debug/kotlin/carbon/compose/icon/IconPreview.kt
@@ -0,0 +1,11 @@
+package carbon.compose.icon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import carbon.compose.icons.ViewOffIcon
+
+@Preview
+@Composable
+private fun IconPreview() {
+    ViewOffIcon()
+}
