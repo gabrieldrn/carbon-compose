7376d535599f5caec7c7d780145a43392ac260c9
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-07-03 23:15:03 -0400

    Move area preview out of input text preview

--- carbon/src/debug/kotlin/carbon/compose/textinput/TextAreaPreview.kt
+++ carbon/src/debug/kotlin/carbon/compose/textinput/TextAreaPreview.kt
@@ -9,46 +9,32 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import carbon.compose.CarbonDesignSystem
 import carbon.compose.foundation.spacing.SpacingScale
 
-internal val loremIpsum =
-    """
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-    """.trimIndent().replace("\n","")
-
-internal class TextInputStatePreviewParameterProvider : PreviewParameterProvider<TextInputState> {
-    override val values: Sequence<TextInputState>
-        get() = TextInputState.entries.asSequence()
-}
-
 @Preview
 @Composable
-private fun EmptyTextInputPreview(
+private fun EmptyTextAreaPreview(
 ) {
     var text by remember {
         mutableStateOf("")
     }
 
     CarbonDesignSystem {
-        TextInput(
+        TextArea(
             label = "Label",
             value = text,
             onValueChange = { text = it },
             modifier = Modifier.padding(SpacingScale.spacing03),
             placeholderText = "Placeholder",
+            maxLines = 7
         )
     }
 }
 
 @Preview
 @Composable
-private fun TextInputPreview(
+private fun TextAreaPreview(
     @PreviewParameter(TextInputStatePreviewParameterProvider::class) state: TextInputState
 ) {
     var text by remember {
@@ -56,7 +42,7 @@ private fun TextInputPreview(
     }
 
     CarbonDesignSystem {
-        TextInput(
+        TextArea(
             label = "Label",
             value = text,
             onValueChange = { text = it },
@@ -64,6 +50,28 @@ private fun TextInputPreview(
             placeholderText = "Placeholder",
             helperText = state.name,
             state = state,
+            maxLines = 7
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TextAreaCounterPreview() {
+    var text by remember {
+        mutableStateOf(loremIpsum)
+    }
+
+    CarbonDesignSystem {
+        TextArea(
+            label = "Label",
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier.padding(SpacingScale.spacing03),
+            placeholderText = "Placeholder",
+            helperText = "With counter",
+            counter = text.split(" ").size to 100,
+            maxLines = 7
         )
     }
 }
