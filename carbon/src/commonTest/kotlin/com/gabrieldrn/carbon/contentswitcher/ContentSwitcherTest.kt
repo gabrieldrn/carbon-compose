ffdbb41220307c8acda4e9049f224f28911e7aff
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-12-27 17:29:10 -0500

    Content switcher layout validation unit test on icon variant

--- carbon/src/commonTest/kotlin/com/gabrieldrn/carbon/contentswitcher/ContentSwitcherTest.kt
+++ carbon/src/commonTest/kotlin/com/gabrieldrn/carbon/contentswitcher/ContentSwitcherTest.kt
@@ -16,18 +16,25 @@
 
 package com.gabrieldrn.carbon.contentswitcher
 
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.test.ComposeUiTest
 import androidx.compose.ui.test.assertAll
 import androidx.compose.ui.test.assertAny
 import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsSelectable
 import androidx.compose.ui.test.assertWidthIsEqualTo
+import androidx.compose.ui.test.hasContentDescription
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onChildren
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.runComposeUiTest
+import com.gabrieldrn.carbon.icons.checkmarkFilledIcon
+import com.gabrieldrn.carbon.icons.closeIcon
+import com.gabrieldrn.carbon.icons.viewIcon
 import com.gabrieldrn.carbon.toList
 import kotlin.test.Test
 
@@ -36,16 +43,10 @@ class ContentSwitcherTest {
     private val stringOptions = listOf("Option 1", "Optiooooon 2", "Option 3")
     private var selectedStringOption = stringOptions.first()
 
-    @Test
-    fun contentSwitcher_default_validateLayout() = runComposeUiTest {
-        setContent {
-            ContentSwitcher(
-                options = stringOptions,
-                selectedOption = selectedStringOption,
-                onOptionSelected = { selectedStringOption = it }
-            )
-        }
+    private lateinit var iconOptions: List<Painter>
+    private lateinit var selectedIconOption: Painter
 
+    private fun ComposeUiTest.validateCommonLayout() {
         onNodeWithTag(ContentSwitcherTestTags.ROOT)
             .assertIsDisplayed()
 
@@ -68,6 +69,19 @@ class ContentSwitcherTest {
                 forEach { it.assertWidthIsEqualTo(expectedWidthForEachButton) }
             }
         }
+    }
+
+    @Test
+    fun contentSwitcher_default_validateLayout() = runComposeUiTest {
+        setContent {
+            ContentSwitcher(
+                options = stringOptions,
+                selectedOption = selectedStringOption,
+                onOptionSelected = { selectedStringOption = it }
+            )
+        }
+
+        validateCommonLayout()
 
         onAllNodesWithTag(
             ContentSwitcherTestTags.BUTTON_CONTENT_ROOT,
@@ -83,6 +97,49 @@ class ContentSwitcherTest {
                         assertAll(
                             hasTestTag(ContentSwitcherTestTags.BUTTON_TEXT) and
                                 hasText(stringOptions[index])
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun contentSwitcher_icon_validateLayout() = runComposeUiTest {
+        val optionsVectors = listOf(checkmarkFilledIcon, closeIcon, viewIcon)
+
+        setContent {
+            optionsVectors
+                .map { rememberVectorPainter(it) }
+                .also {
+                    iconOptions = it
+                    selectedIconOption = it.first()
+                }
+
+            IconContentSwitcher(
+                options = iconOptions,
+                selectedOption = selectedIconOption,
+                onOptionSelected = { selectedIconOption = it },
+                optionsContentDescriptions = iconOptions.zip(optionsVectors.map { it.name }).toMap()
+            )
+        }
+
+        validateCommonLayout()
+
+        onAllNodesWithTag(
+            ContentSwitcherTestTags.BUTTON_CONTENT_ROOT,
+            useUnmergedTree = true
+        ).run {
+            assertCountEquals(stringOptions.size)
+            toList().forEachIndexed { index, node ->
+                with(node) {
+                    assertIsDisplayed()
+                    assertIsSelectable()
+                    onChildren().run {
+                        assertCountEquals(1)
+                        assertAll(
+                            hasTestTag(ContentSwitcherTestTags.BUTTON_IMAGE) and
+                                hasContentDescription(optionsVectors[index].name)
                         )
                     }
                 }
