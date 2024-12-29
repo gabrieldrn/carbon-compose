b5ce70946e8f5821b4eda46f262d3b3f75091fb1
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2024-12-29 15:04:38 -0500

    Fix nav bar persistance on process recreation

--- catalog/src/commonMain/kotlin/com/gabrieldrn/carbon/catalog/Catalog.kt
+++ catalog/src/commonMain/kotlin/com/gabrieldrn/carbon/catalog/Catalog.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -54,7 +55,7 @@ fun Catalog(
     CarbonCatalogTheme {
         val allDestinations = remember { Destination.entries }
 
-        var currentScreen: BaseDestination by remember {
+        var currentScreen: BaseDestination by rememberSaveable {
             mutableStateOf(Destination.Home)
         }
 
