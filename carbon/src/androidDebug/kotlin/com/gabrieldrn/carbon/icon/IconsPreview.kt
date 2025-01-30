335dfb60f4885b80d7198d26cb32fbdf44df82a5
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2025-01-29 23:35:48 -0500

    Change internal icons preview to display them all at once

---
+++ carbon/src/androidDebug/kotlin/com/gabrieldrn/carbon/icon/IconsPreview.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2024 Gabriel Derrien
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gabrieldrn.carbon.icon
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.gabrieldrn.carbon.foundation.spacing.SpacingScale
+import com.gabrieldrn.carbon.icons.CheckmarkFilledIcon
+import com.gabrieldrn.carbon.icons.CloseIcon
+import com.gabrieldrn.carbon.icons.ErrorFilledIcon
+import com.gabrieldrn.carbon.icons.ViewIcon
+import com.gabrieldrn.carbon.icons.ViewOffIcon
+import com.gabrieldrn.carbon.icons.WarningAltIcon
+import com.gabrieldrn.carbon.icons.WarningIcon
+
+private class IconParameterProvider : PreviewParameterProvider<@Composable () -> Unit> {
+    override val values = buildList<@Composable () -> Unit> {
+        add { CheckmarkFilledIcon() }
+        add { CloseIcon() }
+        add { ErrorFilledIcon() }
+        add { ViewIcon() }
+        add { ViewOffIcon() }
+        add { WarningAltIcon() }
+        add { WarningIcon() }
+    }.asSequence()
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+private fun IconPreview(
+    @PreviewParameter(IconParameterProvider::class) icon: @Composable () -> Unit
+) {
+    Box(modifier = Modifier.padding(SpacingScale.spacing03)) {
+        icon()
+    }
+}
