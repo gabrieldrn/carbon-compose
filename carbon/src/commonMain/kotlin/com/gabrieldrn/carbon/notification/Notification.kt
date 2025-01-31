7baa2ac271422da8f67996d907084cb51d63dcda
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2025-01-30 19:54:48 -0500

    Add callout notification parameters with KDoc

--- carbon/src/commonMain/kotlin/com/gabrieldrn/carbon/notification/Notification.kt
+++ carbon/src/commonMain/kotlin/com/gabrieldrn/carbon/notification/Notification.kt
@@ -19,12 +19,33 @@ package com.gabrieldrn.carbon.notification
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
 
+/**
+ * # Callout notification
+ *
+ * Callouts are used to highlight important information contextually within the contents of the
+ * page, and cannot be dismissed. Unlike other notification components they are not triggered by the
+ * user or system, rather they load with the contents of the page. They do not act as a feedback
+ * mechanism, they are persistent, and always present on the screen to provide necessary information
+ * to the user.
+ *
+ * (From [Notification documentation](https://carbondesignsystem.com/components/notification/usage/#callout))
+ *
+ * @param title The title of the notification.
+ * @param body The body of the notification. This can be a simple string or a more complex text
+ * with formatting, allowing for links, bold text, etc.
+ * @param status The status of the notification, which determines its color and icon used.
+ * @param modifier The modifier to apply to the component.
+ */
 @Composable
 public fun CalloutNotification(
+    title: String,
+    body: AnnotatedString,
+    status: NotificationStatus,
     modifier: Modifier = Modifier
 ) {
     Row(modifier = modifier) {
-        
+
     }
 }
