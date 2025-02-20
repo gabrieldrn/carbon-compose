eb53abb8a104a86abaa852bc6b8a7712e43a868d
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2025-02-20 11:57:12 -0400

    Bump version to 0.4.0

--- buildSrc/src/main/kotlin/com/gabrieldrn/carbon/Configuration.kt
+++ buildSrc/src/main/kotlin/com/gabrieldrn/carbon/Configuration.kt
@@ -18,10 +18,10 @@ package com.gabrieldrn.carbon
 
 object Configuration {
     const val majorVersion = 0
-    const val minorVersion = 3
-    const val patchVersion = 1
+    const val minorVersion = 4
+    const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 4
+    const val versionCode = 5
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.github.gabrieldrn"
 }
