8d5e08e467dceb09a8b541802dfa1f4bcdfeeca5
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2025-03-14 23:07:52 -0400

    Change text input demo to use demo screen root composable

--- catalog/src/commonMain/kotlin/com/gabrieldrn/carbon/catalog/textinput/TextInputDemoScreen.kt
+++ catalog/src/commonMain/kotlin/com/gabrieldrn/carbon/catalog/textinput/TextInputDemoScreen.kt
@@ -16,57 +16,34 @@
 
 package com.gabrieldrn.carbon.catalog.textinput
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.gabrieldrn.carbon.Carbon
 import com.gabrieldrn.carbon.button.Button
 import com.gabrieldrn.carbon.button.IconButton
 import com.gabrieldrn.carbon.catalog.Res
-import com.gabrieldrn.carbon.catalog.common.LayerSelectionDropdown
+import com.gabrieldrn.carbon.catalog.common.DemoScreen
 import com.gabrieldrn.carbon.catalog.ic_delete
 import com.gabrieldrn.carbon.catalog.ic_text_long_paragraph
 import com.gabrieldrn.carbon.dropdown.Dropdown
-import com.gabrieldrn.carbon.dropdown.base.dropdownOptionsOf
 import com.gabrieldrn.carbon.dropdown.base.toDropdownOptions
-import com.gabrieldrn.carbon.foundation.color.CarbonLayer
-import com.gabrieldrn.carbon.foundation.color.Layer
-import com.gabrieldrn.carbon.foundation.color.containerBackground
 import com.gabrieldrn.carbon.foundation.spacing.SpacingScale
+import com.gabrieldrn.carbon.tab.TabItem
 import com.gabrieldrn.carbon.textinput.PasswordInput
 import com.gabrieldrn.carbon.textinput.TextArea
 import com.gabrieldrn.carbon.textinput.TextInput
 import com.gabrieldrn.carbon.textinput.TextInputState
 import org.jetbrains.compose.resources.painterResource
 
-private const val TEXT_INPUT_VARIANT = "Text input (single line)"
-private const val TEXT_AREA_VARIANT = "Text area"
-private const val PASSWORD_INPUT_VARIANT = "Password area"
 private val textInputStateOptions = TextInputState.entries.toDropdownOptions()
-private val textInputVariantOptions = dropdownOptionsOf(
-    TEXT_INPUT_VARIANT, TEXT_AREA_VARIANT, PASSWORD_INPUT_VARIANT
-)
+private val textInputVariants = TextInputVariant.entries.map { TabItem(it.label) }
 
 private val loremIpsum =
     """
@@ -77,120 +54,90 @@ private val loremIpsum =
         cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
     """.trimIndent().replace("\n", "")
 
+private enum class TextInputVariant(val label: String) {
+    TextInput("Text input"),
+    TextArea("Text area"),
+    PasswordInput("Password input");
+
+    companion object {
+        fun fromLabel(label: String) = entries.firstOrNull { it.label == label }
+    }
+}
+
 @Composable
 fun TextInputDemoScreen(modifier: Modifier = Modifier) {
-    Column(
+
+    var textInputState by rememberSaveable { mutableStateOf(TextInputState.Enabled) }
+    var text by rememberSaveable { mutableStateOf("") }
+    var passwordHidden by rememberSaveable { mutableStateOf(true) }
+
+    DemoScreen(
+        variants = textInputVariants,
+        defaultVariant = textInputVariants.first(),
+        demoContent = { variantTabItem ->
+            val variant = TextInputVariant.fromLabel(variantTabItem.label)
+
+            LaunchedEffect(variant) {
+                if (variant == TextInputVariant.PasswordInput) {
+                    text = ""
+                }
+            }
+
+            when (variant) {
+                TextInputVariant.PasswordInput -> PasswordInput(
+                    label = "Password input",
+                    value = text,
+                    passwordHidden = passwordHidden,
+                    onValueChange = { text = it },
+                    onPasswordHiddenChange = { passwordHidden = it },
+                    helperText = textInputState.name,
+                    state = textInputState,
+                )
+                TextInputVariant.TextArea -> TextArea(
+                    label = "Text area",
+                    value = text,
+                    onValueChange = { text = it },
+                    placeholderText = "Placeholder",
+                    helperText = textInputState.name,
+                    state = textInputState,
+                    maxLines = 5
+                )
+                else -> TextInput(
+                    label = "Text input",
+                    value = text,
+                    onValueChange = { text = it },
+                    placeholderText = "Placeholder",
+                    helperText = textInputState.name,
+                    state = textInputState,
+                )
+            }
+        },
+        demoParametersContent = { variantTabItem ->
+            val variant = TextInputVariant.fromLabel(variantTabItem.label)
+
+            Dropdown(
+                label = "State",
+                placeholder = "Choose an option",
+                options = textInputStateOptions,
+                selectedOption = textInputState,
+                onOptionSelected = { textInputState = it },
+            )
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Button(
+                    label = "Lorem ipsum",
+                    onClick = { text = loremIpsum },
+                    iconPainter = painterResource(Res.drawable.ic_text_long_paragraph),
+                    isEnabled = variant != TextInputVariant.PasswordInput,
+                    modifier = Modifier.weight(1f)
+                )
+                IconButton(
+                    iconPainter = painterResource(Res.drawable.ic_delete),
+                    onClick = { text = "" },
+                    modifier = Modifier.padding(start = SpacingScale.spacing03)
+                )
+            }
+        },
         modifier = modifier
-            .fillMaxSize()
-            .containerBackground()
-            .verticalScroll(state = rememberScrollState())
-            .padding(WindowInsets.navigationBars.asPaddingValues())
-    ) {
-        var layer by rememberSaveable { mutableStateOf(Layer.Layer00) }
-        var textInputState by rememberSaveable { mutableStateOf(TextInputState.Enabled) }
-        var variant by rememberSaveable { mutableStateOf(TEXT_INPUT_VARIANT) }
-        var text by rememberSaveable { mutableStateOf("") }
-        var passwordHidden by rememberSaveable { mutableStateOf(true) }
-
-        LaunchedEffect(variant) {
-            if (variant == PASSWORD_INPUT_VARIANT) {
-                text = ""
-            }
-        }
-
-        CarbonLayer(layer = layer) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .containerBackground()
-                    .padding(SpacingScale.spacing05)
-            ) {
-                when (variant) {
-                    TEXT_INPUT_VARIANT -> TextInput(
-                        label = "Text input",
-                        value = text,
-                        onValueChange = { text = it },
-                        modifier = Modifier.align(Alignment.Center),
-                        placeholderText = "Placeholder",
-                        helperText = textInputState.name,
-                        state = textInputState,
-                    )
-                    PASSWORD_INPUT_VARIANT -> PasswordInput(
-                        label = "Password input",
-                        value = text,
-                        passwordHidden = passwordHidden,
-                        onValueChange = { text = it },
-                        onPasswordHiddenChange = { passwordHidden = it },
-                        modifier = Modifier.align(Alignment.Center),
-                        helperText = textInputState.name,
-                        state = textInputState,
-                    )
-                    TEXT_AREA_VARIANT -> TextArea(
-                        label = "Text area",
-                        value = text,
-                        onValueChange = { text = it },
-                        modifier = Modifier.align(Alignment.Center),
-                        placeholderText = "Placeholder",
-                        helperText = textInputState.name,
-                        state = textInputState,
-                        maxLines = 5
-                    )
-                }
-            }
-        }
-
-        CarbonLayer {
-            Column(
-                modifier = Modifier
-                    .padding(SpacingScale.spacing05)
-                    .containerBackground()
-                    .padding(SpacingScale.spacing05),
-                verticalArrangement = Arrangement.spacedBy(SpacingScale.spacing05)
-            ) {
-                Dropdown(
-                    label = "Variant / modifier",
-                    placeholder = "Choose a variant",
-                    options = textInputVariantOptions,
-                    selectedOption = variant,
-                    onOptionSelected = { variant = it },
-                )
-
-                Spacer(
-                    modifier = Modifier
-                        .background(color = Carbon.theme.borderStrong01)
-                        .fillMaxWidth()
-                        .height(1.dp)
-                )
-
-                Dropdown(
-                    label = "State",
-                    placeholder = "Choose an option",
-                    options = textInputStateOptions,
-                    selectedOption = textInputState,
-                    onOptionSelected = { textInputState = it },
-                )
-
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    Button(
-                        label = "Lorem ipsum",
-                        onClick = { text = loremIpsum },
-                        iconPainter = painterResource(Res.drawable.ic_text_long_paragraph),
-                        isEnabled = variant != PASSWORD_INPUT_VARIANT,
-                        modifier = Modifier.weight(1f)
-                    )
-                    IconButton(
-                        iconPainter = painterResource(Res.drawable.ic_delete),
-                        onClick = { text = "" },
-                        modifier = Modifier.padding(start = SpacingScale.spacing03)
-                    )
-                }
-
-                LayerSelectionDropdown(
-                    selectedLayer = layer,
-                    onLayerSelected = { layer = it },
-                )
-            }
-        }
-    }
+    )
 }
