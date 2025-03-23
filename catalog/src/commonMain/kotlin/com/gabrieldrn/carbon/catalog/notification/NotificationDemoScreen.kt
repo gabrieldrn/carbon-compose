0065320ab3d40a9e2625b9ea5625e0d4a15cd984
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2025-03-23 00:14:26 -0400

    Move callout notification demo to standalone file

--- catalog/src/commonMain/kotlin/com/gabrieldrn/carbon/catalog/notification/NotificationDemoScreen.kt
+++ catalog/src/commonMain/kotlin/com/gabrieldrn/carbon/catalog/notification/NotificationDemoScreen.kt
@@ -17,67 +17,33 @@
 package com.gabrieldrn.carbon.catalog.notification
 
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.LinkAnnotation
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextLinkStyles
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextDecoration
-import com.gabrieldrn.carbon.Carbon
 import com.gabrieldrn.carbon.catalog.common.DemoScreen
 import com.gabrieldrn.carbon.dropdown.Dropdown
 import com.gabrieldrn.carbon.dropdown.base.toDropdownOptions
-import com.gabrieldrn.carbon.foundation.spacing.SpacingScale
-import com.gabrieldrn.carbon.notification.CalloutNotification
 import com.gabrieldrn.carbon.notification.NotificationStatus
+import com.gabrieldrn.carbon.tab.TabItem
 import com.gabrieldrn.carbon.textinput.TextInput
 import com.gabrieldrn.carbon.toggle.Toggle
 
-@Composable
-fun NotificationDemoScreen(modifier: Modifier = Modifier) {
+private enum class NotificationVariant {
+    Callout,
+    Inline
+}
 
+private val variants = NotificationVariant.entries.map { TabItem(it.name) }
+
+@Composable
+fun NotificationDemoScreen(
+    modifier: Modifier = Modifier
+) {
     var notificationStatus by remember { mutableStateOf(NotificationStatus.Success) }
     var highContrast by remember { mutableStateOf(false) }
-
-    val urlDemoNotification: @Composable ColumnScope.() -> Unit = {
-        val url = remember {
-            "https://carbondesignsystem.com/components/notification/usage/#callout"
-        }
-        CalloutNotification(
-            title = "Callout notification",
-            body = buildAnnotatedString {
-                val clickableText = "callout notification"
-                val string = "This is a $clickableText."
-                append(string)
-                addLink(
-                    url = LinkAnnotation.Url(
-                        url = url,
-                        styles = TextLinkStyles(
-                            style = SpanStyle(
-                                color = Carbon.theme.linkPrimary,
-                                textDecoration = TextDecoration.Underline
-                            )
-                        ),
-                    ),
-                    start = string.indexOf(clickableText),
-                    end = string.indexOf(clickableText) + clickableText.length
-                )
-            },
-            status = notificationStatus,
-            highContrast = highContrast,
-            modifier = Modifier.width(IntrinsicSize.Max)
-        )
-    }
-
     var title by remember {
         mutableStateOf("Lorem ipsum")
     }
@@ -85,17 +51,7 @@ fun NotificationDemoScreen(modifier: Modifier = Modifier) {
         mutableStateOf("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
     }
 
-    val editableDemoNotification: @Composable ColumnScope.() -> Unit = {
-        CalloutNotification(
-            title = title,
-            body = body,
-            status = notificationStatus,
-            highContrast = highContrast,
-            modifier = Modifier.width(IntrinsicSize.Max)
-        )
-    }
-
-    val parametersContent: @Composable ColumnScope.() -> Unit = {
+    val parametersContent: @Composable ColumnScope.(TabItem) -> Unit = {
         TextInput(
             label = "Title",
             value = title,
@@ -126,13 +82,15 @@ fun NotificationDemoScreen(modifier: Modifier = Modifier) {
     }
 
     DemoScreen(
+        variants = variants,
         modifier = modifier,
         demoContent = {
-            urlDemoNotification()
-
-            Spacer(modifier = Modifier.height(SpacingScale.spacing05))
-
-            editableDemoNotification()
+            CalloutNotificationDemo(
+                title = title,
+                body = body,
+                notificationStatus = notificationStatus,
+                highContrast = highContrast
+            )
         },
         demoParametersContent = parametersContent,
         displayVariantsWIPNotification = true
