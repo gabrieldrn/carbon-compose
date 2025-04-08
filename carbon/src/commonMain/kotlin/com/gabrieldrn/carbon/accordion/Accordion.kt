1f31c83f07e6adb06393dfc9815bd989f4dd3405
Gabriel Derrien <gabriel.derrien+work@gmail.com>
2025-04-08 19:50:53 -0400

    Add Accordion composable base

---
+++ carbon/src/commonMain/kotlin/com/gabrieldrn/carbon/accordion/Accordion.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2025 Gabriel Derrien
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gabrieldrn.carbon.accordion
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * # Accordion
+ *
+ * The accordion component delivers large amounts of content in a small space through progressive
+ * disclosure. The header title gives the user a high level overview of the content allowing the
+ * user to decide which sections to read.
+ *
+ * Accordions can make information processing and discovering more effective. However, it does hide
+ * content from users and it’s important to account for a user not noticing or reading all of the
+ * included content.
+ *
+ * (From [Accordion documentation](https://carbondesignsystem.com/components/accordion/usage/))
+ */
+@Composable
+public fun Accordion(
+    modifier: Modifier = Modifier
+) {
+    // TODO
+}
